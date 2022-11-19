ebdb0197d4e32f90bbc9bee51a30ce7f7ad8c1ad
michael-mbugua <michael.mbugua@student.moringaschool.com>
2022-11-19 11:38:37 +0300

    apply grid in products

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "efa72a22d27b3ce5bc99"; }
+/******/ 		__webpack_require__.h = function() { return "841c924617782be4ab05"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -4,8 +4,9 @@ import { useEffect } from 'react'
 import Product from './Product'
 function ProductFeed({ products }) {
     return (
+        
         <div className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto'>
-            {products.map(({id,title,image,description,price,category})=>(
+            {products.slice(0,4).map(({id,title,image,description,price,category})=>(
                 <Product 
                 key={id}
                 id={id}
@@ -16,7 +17,32 @@ function ProductFeed({ products }) {
                 image={image}
                 />
             ))}
-            
+            <img className='md:col-span-full' src="https://links.papareact.com/dyz" alt="" />
+            <div className='md:col-span-1'>
+                {products.slice(4,5)
+                .map(({id,title,image,description,price,category})=>(
+                    <Product 
+                    key={id}
+                    id={id}
+                    title={title}
+                    price={price}
+                    description={description}
+                    category={category}
+                    image={image}
+                    />
+            ))}
+            </div>
+            {products.slice(5,products.length).map(({id,title,image,description,price,category})=>(
+                <Product 
+                key={id}
+                id={id}
+                title={title}
+                price={price}
+                description={description}
+                category={category}
+                image={image}
+                />
+            ))}
         </div>
     
     )
